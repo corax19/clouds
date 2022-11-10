ad088125475b26d7e165d938c383210f16776dc4
root <agassiev1974@gmail.com>
2022-11-10 08:59:14 +0000

    OK 10112022 2

--- app/models/moh.rb
+++ app/models/moh.rb
@@ -3,6 +3,6 @@ belongs_to :account
 
 validates_uniqueness_of :name, scope: :account_id
 
-has_many :mohentries, dependent: :destroy
+has_many :moh_entries, dependent: :destroy
 
 end
--- app/models/sound.rb
+++ app/models/sound.rb
@@ -5,7 +5,7 @@ class Sound < ApplicationRecord
 belongs_to :account
 
 validates_uniqueness_of :name, scope: :account_id
-#has_many :mohentries, dependent: :destroy
+has_many :moh_entries, dependent: :destroy
 
 mount_uploader :audio, AudioUploader
 
